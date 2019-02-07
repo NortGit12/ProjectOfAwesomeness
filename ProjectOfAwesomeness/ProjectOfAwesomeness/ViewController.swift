2d8d535b865ee683a799c0d253ce49f5fd0a5d80
Jeff Norton <jeff12ntn@gmail.com>
2019-02-07 13:46:08 -0700

    Contains the initial project creation

---
+++ ProjectOfAwesomeness/ProjectOfAwesomeness/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProjectOfAwesomeness
+//
+//  Created by Jeff Norton on 2/7/19.
+//  Copyright © 2019 Oh Yeah, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
